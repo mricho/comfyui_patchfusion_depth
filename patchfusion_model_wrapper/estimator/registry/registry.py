5bb0f528712ad051068d39bda4b713677a5f0650
Michael <3901301+mricho@users.noreply.github.com>
2025-10-28 16:22:58 -0500

    registry progress

--- patchfusion_model_wrapper/estimator/registry/registry.py
+++ patchfusion_model_wrapper/estimator/registry/registry.py
@@ -4,5 +4,17 @@ from mmengine import Registry
 from mmengine.registry import MODELS as MMENGINE_MODELS
 from mmengine.registry import DATASETS as MMENGINE_DATASETS
 
-MODELS = Registry('model', parent=MMENGINE_MODELS, locations=['patchfusion_model_wrapper.estimator.models'])
-DATASETS = Registry('dataset', parent=MMENGINE_DATASETS, locations=['patchfusion_model_wrapper.estimator.datasets'])
+# Check if registries already exist in the parent registry
+_scope_name = 'patchfusion_model_wrapper'
+
+# Check if MODELS registry already exists
+if _scope_name in MMENGINE_MODELS.children:
+    MODELS = MMENGINE_MODELS.children[_scope_name]
+else:
+    MODELS = Registry('model', parent=MMENGINE_MODELS, locations=['patchfusion_model_wrapper.estimator.models'], scope=_scope_name)
+
+# Check if DATASETS registry already exists  
+if _scope_name in MMENGINE_DATASETS.children:
+    DATASETS = MMENGINE_DATASETS.children[_scope_name]
+else:
+    DATASETS = Registry('dataset', parent=MMENGINE_DATASETS, locations=['patchfusion_model_wrapper.estimator.datasets'], scope=_scope_name)
